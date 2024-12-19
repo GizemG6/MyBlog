90f020f52dd80b6a66183fd6513ae8b63135655e
cang8 <0.gizemgunes@gmail.com>
2024-12-19 17:11:34 +0300

    added-MyBlog.Dto

--- Frontend/MyBlog.WebUI/Controllers/LoginController.cs
+++ Frontend/MyBlog.WebUI/Controllers/LoginController.cs
@@ -4,6 +4,13 @@ namespace MyBlog.WebUI.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
